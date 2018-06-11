c11cbafa0ef420599f15e591d3d30e4465564a75
thjjava <526040147@qq.com>
2018-06-11 10:00:48 +0800

    2

--- WebRoot/js/page/company.js
+++ WebRoot/js/page/company.js
@@ -153,7 +153,7 @@ function deleteobj(){
 						ids+="_"+rows[i].id;
 					}
 				}
-				$.post(path+"/company_deletebyids.do",{"ids":ids},function(data){ 　　
+				$.post(path+"/company_deletebyids.do",{"ids":ids},function(data){
 					if("success"==data){
 						$('#'+render).datagrid('clearSelections');
 			     		$.messager.alert('提示',"更新数据成功!");
